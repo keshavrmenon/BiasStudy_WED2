f52842f8ed8d596d7c6c1b15f297559d654ee2c2
keshavrmenon <keshavmenon@iisc.ac.in>
2021-03-03 10:52:17 +0530

    bug fixes

--- runjobs_NMSSM.sh
+++ runjobs_NMSSM.sh
@@ -9,7 +9,7 @@ for pdfidx in {0..8}
 do
 for iter in {0..3}
 do
-        echo "source runBias_.sh Radion $mass $icat $pdfidx $(($s0+$iter)) $(($s1+$iter))"
+        echo "source runBias_NMSSM.sh NMSSM $mass $icat $pdfidx $(($s0+$iter)) $(($s1+$iter))"
 	sh runBias_NMSSM.sh NMSSM $mass $icat $pdfidx $(($s0+$iter)) $(($s1+$iter))
 done
 done
